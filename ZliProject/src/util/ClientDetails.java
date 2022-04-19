1de34df9f56315654cbbd3de99e593ec7d641ade
YanaRaitsin <yanaraitsin4@gmail.com>
2022-04-19 20:27:44 +0300

    server table fixed

--- ZliProject/src/util/ClientDetails.java
+++ ZliProject/src/util/ClientDetails.java
@@ -1,18 +1,33 @@
 package util;
-public class ClientDetails {
-		private String ip;
-		private String client;
-		private String status;
 
-		public ClientDetails(String ip, String client, String status) {
-			this.ip = ip;
-			this.client = client;
-			this.status = status;
-		}
-		
-		public String getClient() { return client; }
-		
-		public void setStatus(String status) {this.status = status;}
-		
-		public String toString() {return ("This client details: Server IP: " + ip + " Client IP: " + client + " Status: " + status);} 
+public class ClientDetails {
+	private String ip;
+	private String client;
+	private String status;
+
+	public ClientDetails(String ip, String client, String status) {
+		this.ip = ip;
+		this.client = client;
+		this.status = status;
 	}
+
+	public String getClient() {
+		return client;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String toString() {
+		return ("This client details: Server IP: " + ip + " Client IP: " + client + " Status: " + status);
+	}
+}
