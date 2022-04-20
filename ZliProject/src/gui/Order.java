6ba797bd7582045276cf20488710cdc3c5be2712
EitanGerman <eitan.ger@gmail.com>
2022-04-20 11:50:40 +0300

    fix order class

--- ZliProject/src/gui/Order.java
+++ ZliProject/src/gui/Order.java
@@ -1,84 +1,50 @@
 package gui;
 
-import javafx.beans.property.SimpleStringProperty;
-
 public class Order {
-	SimpleStringProperty orderNumber, price, greetingCard, color, dOrder, shop, date, orderDate;
-
-	Order(String orderNumber, String price, String greetingCard, String color, String dOrder, String shop, String date,
-			String orderDate) {
-		this.orderNumber = new SimpleStringProperty(orderNumber);
-		this.price = new SimpleStringProperty(price);
-		this.greetingCard = new SimpleStringProperty(greetingCard);
-		this.color = new SimpleStringProperty(color);
-		this.dOrder = new SimpleStringProperty(dOrder);
-		this.shop = new SimpleStringProperty(shop);
-		this.date = new SimpleStringProperty(date);
-		this.orderDate = new SimpleStringProperty(orderDate);
+	private String orderNumber, price, greetingCard, color, dOrder, shop, date, orderDate;
+	
+	public Order(String orderNumber, String price, String greetingCard, String color, String dOrder, String shop,
+			String date, String orderDate) {
+		this.orderNumber = orderNumber;
+		this.price = price;
+		this.greetingCard = greetingCard;
+		this.color = color;
+		this.dOrder = dOrder;
+		this.shop = shop;
+		this.date = date;
+		this.orderDate = orderDate;
 	}
 
-	public SimpleStringProperty getOrderNumber() {
+	public String getOrderNumber() {
 		return orderNumber;
 	}
 
-	public void setOrderNumber(SimpleStringProperty orderNumber) {
-		this.orderNumber = orderNumber;
-	}
-
-	public SimpleStringProperty getPrice() {
+	public String getPrice() {
 		return price;
 	}
 
-	public void setPrice(SimpleStringProperty price) {
-		this.price = price;
-	}
-
-	public SimpleStringProperty getGreetingCard() {
+	public String getGreetingCard() {
 		return greetingCard;
 	}
 
-	public void setGreetingCard(SimpleStringProperty greetingCard) {
-		this.greetingCard = greetingCard;
-	}
-
-	public SimpleStringProperty getColor() {
+	public String getColor() {
 		return color;
 	}
 
-	public void setColor(SimpleStringProperty color) {
-		this.color = color;
-	}
-
-	public SimpleStringProperty getdOrder() {
+	public String getdOrder() {
 		return dOrder;
 	}
 
-	public void setdOrder(SimpleStringProperty dOrder) {
-		this.dOrder = dOrder;
-	}
-
-	public SimpleStringProperty getShop() {
+	public String getShop() {
 		return shop;
 	}
 
-	public void setShop(SimpleStringProperty shop) {
-		this.shop = shop;
-	}
-
-	public SimpleStringProperty getDate() {
+	public String getDate() {
 		return date;
 	}
 
-	public void setDate(SimpleStringProperty date) {
-		this.date = date;
-	}
-
-	public SimpleStringProperty getOrderDate() {
+	public String getOrderDate() {
 		return orderDate;
-	}
-
-	public void setOrderDate(SimpleStringProperty orderDate) {
-		this.orderDate = orderDate;
 	}
 
 }
