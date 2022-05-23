92b64824f7d4785c5247a222a2735f166f70130d
Eitan German <99746116+EitanGerman@users.noreply.github.com>
2022-05-23 14:09:19 +0300

    improvements to notification system, in the userHomeScreen notifications now aouto update every 30 seconds

--- zli_client/src/home/HomeUserTypesController.java
+++ zli_client/src/home/HomeUserTypesController.java
@@ -4,9 +4,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import client.ClientFormController;
 import entities.User;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,6 +19,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 import notifications.Notification;
 import notifications.NotificationController;
 import util.Commands;
@@ -22,82 +27,96 @@ import util.ManageClients;
 import util.ManageScreens;
 import util.Screens;
 
-public class HomeUserTypesController implements HomeInterface,Initializable {
-	private ArrayList<Notification> notifications = new ArrayList<>(); 
-	
-    @FXML
-    private ImageView notificationIcon;
+public class HomeUserTypesController implements HomeInterface, Initializable {
+	private ArrayList<Notification> notifications = new ArrayList<>();
+	Timeline notificationThread;
+	@FXML
+	private ImageView notificationIcon;
 
-    @FXML
-    private Label notificationLabel;
-    
-    @FXML
-    private Button exitButton;
+	@FXML
+	private Label notificationLabel;
 
-    @FXML
-    private GridPane gridOptions;
+	@FXML
+	private Button exitButton;
 
-    @FXML
-    private HBox hbox;
+	@FXML
+	private GridPane gridOptions;
 
-    @FXML
-    private Button logoutButton;
+	@FXML
+	private HBox hbox;
 
-    @FXML
-    private Label userNameLabel;
-    
-    private ArrayList<HomeVBox> buttons = new ArrayList<>();
-    
+	@FXML
+	private Button logoutButton;
+
+	@FXML
+	private Label userNameLabel;
+
+	private ArrayList<HomeVBox> buttons = new ArrayList<>();
+
 	@Override
 	public void exitHomeScreen(MouseEvent event) {
-		//release the client from the ocsf server + disconnect from the db
+		// release the client from the ocsf server + disconnect from the db
 		ManageClients.exitClient();
-		//exit window
+		// exit window
 		System.exit(0);
 	}
 
 	@Override
 	public void logoutFromUser(MouseEvent event) {
 		User.getUserInstance().logout();
-		ManageScreens.changeScreenTo(Screens.GUEST_HOME);	
+		ManageScreens.changeScreenTo(Screens.GUEST_HOME);
 	}
-	
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		ArrayList<Screens> userScreens = ManageClients.getUserScreens(User.getUserInstance().getType());
-		setScreen(userScreens);// 
+		setScreen(userScreens);//
 		userNameLabel.setText(User.getUserInstance().getUsername());
-		
+		getNotifications();
+		//update notifications every 30 seconds
+		notificationThread = new Timeline(new KeyFrame(Duration.seconds(30.0), e -> {
+			getNotifications();
+		}));
+		notificationThread.setCycleCount(Timeline.INDEFINITE);
+		notificationThread.play();
+
+//				HashMap<String, Object> message = new HashMap<>();
+//				message.put("command", Commands.FETCH_NOTIFICATIONS);
+//				message.put("idUser", User.getUserInstance().getIdUser());
+//				Object response = ClientFormController.client.accept(message);
+//				notifications = (ArrayList<Notification>) response;
+//				notificationLabel.setText("" + notifications.size());
+	}
+
+	private void getNotifications() {
 		HashMap<String, Object> message = new HashMap<>();
 		message.put("command", Commands.FETCH_NOTIFICATIONS);
 		message.put("idUser", User.getUserInstance().getIdUser());
 		Object response = ClientFormController.client.accept(message);
-		notifications = (ArrayList<Notification>)response;
-		notificationLabel.setText(""+notifications.size());
+		notifications = (ArrayList<Notification>) response;
+		notificationLabel.setText("" + notifications.size());
 	}
 
-	private void setScreen(ArrayList<Screens> userScreens) {// 
+	private void setScreen(ArrayList<Screens> userScreens) {//
 		if (userScreens != null)
-			for (int i = 0; i < userScreens.size();i++) {
+			for (int i = 0; i < userScreens.size(); i++) {
 				if (i / 3 > 0 && i % 3 == 0)
-					gridOptions.addRow(i/3);
+					gridOptions.addRow(i / 3);
 				buttons.add(new HomeVBox(userScreens.get(i)));
-				this.gridOptions.add(buttons.get(i), (i+2) % 3, (i+2) / 3);
+				this.gridOptions.add(buttons.get(i), (i + 2) % 3, (i + 2) / 3);
 			}
 	}
-	
 
-    @FXML
-    void showNotifications(MouseEvent event) {
-    	try {
-    		NotificationController.notifications = notifications;
-			ManageScreens.openPopupFXML(NotificationController.class.getResource("NotificationMainScreen.fxml"), "Notification Center");
+	@FXML
+	void showNotifications(MouseEvent event) {
+		try {
+			NotificationController.notifications = notifications;
+			ManageScreens.openPopupFXML(NotificationController.class.getResource("NotificationMainScreen.fxml"),
+					"Notification Center");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-
+	}
 
 }
--- zli_server/src/util/ServerNotificationManager.java
+++ zli_server/src/util/ServerNotificationManager.java
@@ -4,7 +4,7 @@ import notifications.NotificationManager;
 
 public class ServerNotificationManager extends NotificationManager{
 	
-	public static boolean sendNotification(int idUser,NotificationType nt, int param) {
+	public static boolean sendNotification(int idUser,NotificationType nt, Object param) {
 		String notification = getNotification(nt,param);
 		return AnalayzeCommand.sendNotification(idUser, notification);
 	}
