a74c0dbb248381c5e875f596c537c9841fd635c4
Eitan German <99746116+EitanGerman@users.noreply.github.com>
2022-06-05 20:12:06 +0300

    fixing view details in orders

--- zli_client/src/home/HomeVBox.java
+++ zli_client/src/home/HomeVBox.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import util.ManageScreens;
@@ -17,33 +18,33 @@ import util.Screens;
  */
 public class HomeVBox extends VBox {
 	private ImageView image;
-	private Button button;
+	private Label cardName;
 	private Screens changeToScreen;
 
 	public HomeVBox(Screens changeToScreen) {
+		this.setId("buttonCard");
 		this.changeToScreen = changeToScreen;
-		button = new Button();
-		button.setCursor(Cursor.HAND);
-		button.setText(ManageScreens.getName(changeToScreen));
+		cardName = new Label();
+		cardName.setId("cardLabel");
+		cardName.setText(ManageScreens.getName(changeToScreen));
 		if (!ManageScreens.getIconPath(changeToScreen).equals(""))
 			image = new ImageView(ManageScreens.getIconPath(changeToScreen));
 		init();
 	}
 
 	/**
-	 * Setting Buttons and Icon
+	 * Setting label and Icon
 	 */
 	public void init() {
-		this.setStyle("-fx-background-color : LAVENDER; -fx-background-radius :20");
 		this.setPadding(new Insets(10));
 		this.setSpacing(10);
-		this.getChildren().add(button);
+		this.getChildren().add(cardName);
 		if (image != null) {
 			image.setFitWidth(150);
 			image.setFitHeight(150);
 			this.getChildren().add(image);
 		}
-		button.setOnMouseClicked(new EventHandler<Event>() {
+		this.setOnMouseClicked(new EventHandler<Event>() {
 
 			@Override
 			public void handle(Event event) {
